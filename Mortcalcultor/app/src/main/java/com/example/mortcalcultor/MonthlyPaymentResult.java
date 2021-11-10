c903a5d8a0e218c3350fc3ae5a65fe2fb48e1d0a
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-11-10 15:34:48 -0500

    update result screen

--- Mortcalcultor/app/src/main/java/com/example/mortcalcultor/MonthlyPaymentResult.java
+++ Mortcalcultor/app/src/main/java/com/example/mortcalcultor/MonthlyPaymentResult.java
@@ -5,25 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MonthlyPaymentResult extends AppCompatActivity {
     private TextView payment;
+    private Button recalculateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_payment_result);
 
-
         //Get element by ID
         payment = findViewById(R.id.payment);
-
+        recalculateBtn= findViewById(R.id.recalculate);
+        //Get data from intent
         Intent intent = getIntent();
         String monthlyPayment = intent.getStringExtra("monthlyPayment");
+        //Set text for payment view
         payment.setText("$"+monthlyPayment);
-        Log.i("mon", "onCreate: " + monthlyPayment);
+
+        //Add click event to recalculate button
+        recalculateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MonthlyPaymentResult.this, MonthlyPaymentCalActivity.class);
+                startActivity(intent1);
+            }
+        });
 
     }
 }
